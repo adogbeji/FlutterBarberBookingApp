de5f1245690fd49c04a19ba376572db0f57d8484
adogbeji <baka_1993@yahoo.co.uk>
2024-12-12 15:24:34 +0000

    Tweak button styling in onboarding screen

--- lib/views/screens/onboarding_screen.dart
+++ lib/views/screens/onboarding_screen.dart
@@ -17,14 +17,17 @@ class _OnboardingScreenState extends State<OnboardingScreen> {
         child: Column(
           children: [
             Image.asset('assets/images/barber.png'),
+            const SizedBox(height: 80,),
             Container(
+              padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
               decoration: BoxDecoration(
                 color: Colors.orange,
+                borderRadius: BorderRadius.circular(30),
               ),
               child: const Text(
                 'Get a stylish haircut',
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 22,
                   color: Colors.white,
                   fontWeight: FontWeight.bold,
                 ),
